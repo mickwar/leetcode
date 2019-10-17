0262f9f34fed4dbf0b56e958c9ba8af6a05318c4
mickey <mickey.a.warner@gmail.com>
2019-10-17 15:58:58 -0600

    more compact solution

--- medium/jump_game.py
+++ medium/jump_game.py
@@ -3,31 +3,15 @@
 #   performed 95% faster
 #   used less than 7% memory
 # than other solutions
-# 
-
 
 class Solution:
     def canJump(self, nums):
-        
-        if len(nums) == 1:
-            return True
-        
-        # Get index for largest jump that can be made
-        last_index = len(nums) - 1
-        max_jump = [i + nums[i] for i in range(last_index)]
-        
-        if max(max_jump) < last_index:
-            return False
-        
-        # Where the final jump can be made
-        index = next(ind for ind, val in enumerate(max_jump) if val >= last_index) 
-        
-        # Now work backwards
-        for i in range(index - 1, -1, -1):
-            if max_jump[i] >= index:
-                index = i
-            
-        if index == 0:
-            return True
-        else:
-            return False
+
+        # Work backwords, always determining where a successful
+        # jump may have originated
+        index = len(nums) - 1
+        for i in range(index, -1, -1):
+            index = i if i + nums[i] >= index else index
+
+        # Sequence of jumps can be made if index arrives at 0
+        return True if index == 0 else False
