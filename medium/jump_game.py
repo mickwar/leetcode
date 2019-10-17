39975c866c35442a37c93023f966876c5e1f065a
mickey <mickey.a.warner@gmail.com>
2019-10-17 14:52:24 -0600

    add code

---
+++ medium/jump_game.py
@@ -0,0 +1,31 @@
+# https://leetcode.com/problems/jump-game/
+#
+# This solution works, but takes too long
+
+class Solution:
+    def canJump(self, nums: List[int]) -> bool:
+        
+        if len(nums) == 1:
+            return True
+        
+        index = 0
+        jump_path = [0]
+        while nums[0] > 0:
+            # Check if cleared
+            if index + nums[index] >= len(nums) - 1:
+                return True
+            
+            # Do another jump (if possible)
+            if nums[index] > 0:
+                index = index + nums[index]
+                jump_path.append(index)
+            else:
+                # Otherwise, go back a step in jump_path
+                # And decrement the value in nums
+                # (This ensures we look at the largest jump posssible and
+                # work our way down until no more jumps)
+                jump_path.pop()
+                index = jump_path[-1]
+                nums[index] -= 1
+                
+        return False
