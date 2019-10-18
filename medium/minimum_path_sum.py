a7f42ab38f0cc4e3caf68a03fe54b1ac830ad7ea
mickey <mickey.a.warner@gmail.com>
2019-10-18 11:38:07 -0600

    add recursive solution

--- medium/minimum_path_sum.py
+++ medium/minimum_path_sum.py
@@ -1,33 +1,43 @@
 # https://leetcode.com/problems/minimum-path-sum/
 
-
-# This is an incorrect solution
+# Thanks to this article for help: https://www.geeksforgeeks.org/min-cost-path-dp-6/
+# Naive recursion, works but takes too long
+import sys
 class Solution:
     def minPathSum(self, grid):
-        # Greedy search (always choose the lowest value)
         n = len(grid)       # rows
         m = len(grid[0])    # columns
-        x = 0               # position in path
-        y = 0               # position in path
-        out = 0
+        # Working our way backward, from the end to the start
+        return self.minCostToXY(grid, n - 1, m - 1)
         
-        while x < n - 1 or y < m - 1:
-            if x == n - 1:
-                out += grid[x][y+1]
-                y += 1
-            elif y == m - 1:
-                out += grid[x+1][y]
-                x += 1
-            else:
-                if grid[x][y+1] > grid[x+1][y]:
-                    out += grid[x+1][y]
-                    x += 1
-                else:
-                    out += grid[x][y+1]
-                    y += 1
-                    
-        # Add the last cell
-        out += grid[x][y]
-                    
-        return out
-    
+    def minCostToXY(self, grid, x, y):
+        # Return very large value if out of bounds
+        if x < 0 or y < 0:
+            return sys.maxsize
+        elif x == 0 and y == 0:
+            return grid[x][y]
+        else:
+            # We can only go down and right, hence the two branches.
+            # This is where the recursion goes down. It needs to follow each
+            # path until it can come back up and compute the minimums.
+            return grid[x][y] + min(self.minCostToXY(grid, x, y-1),
+                                    self.minCostToXY(grid, x-1, y))
+
+
+
+obj = Solution()
+
+grid = [
+  [1,3,1,0],
+  [1,5,1,0],
+  [4,2,1,1]
+]
+print(obj.minPathSum(grid))
+
+
+grid = [
+  [1,3,1,0],
+  [1,5,1,0],
+  [0,0,1,1]
+]
+print(obj.minPathSum(grid))
